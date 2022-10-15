5d1ab4826b0363df469a93d349cf299555689598
yosle <yosle007@gmail.com>
2022-10-15 19:50:48 -0400

    changes for project folder structure

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 import dts from "rollup-plugin-dts";
 const config = [
     {
-        input: 'dist/index.js',
+        input: 'src/dist/index.js',
         output: {
             file: 'index.js',
             format: 'cjs',
@@ -12,7 +12,7 @@ const config = [
         external: ['axios'],
         plugins: [typescript()]
     }, {
-        input: 'dist/index.d.ts',
+        input: 'src/dist/index.d.ts',
         output: {
             file: 'index.d.ts',
             format: 'es'
