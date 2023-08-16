2c4e14c4cc3bdc869d3652e86fce43eded1c13d1
yosle <yosle007@gmail.com>
2023-08-16 17:25:12 -0400

    change header comment

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 /**
- * Tropipayjs is a Typescript/Javascript library for the Tropipay API.
+ * TropipayJS is a powerful TypeScript/JavaScript library designed to provide seamless interaction
+ * with the Tropipay API. It simplifies the process of integrating Tropipay's functionality into
+ * your applications.
  *
- * @author Yosleivy Baez Acosta
+ * Developed by Yosleivy Baez Acosta
+ * GitHub: https://github.com/yosle/tropipayjs
  *
+ * @version 0.1.11
+ * @license MIT
  */
+
 export { Tropipay } from "./api/TropipayAPI";
 export { ClientSideUtils } from "./api/TropipayAPI";
 export { ServerSideUtils } from "./utils/TropipayUtils";
