c35b5d4f8b10200038a1e78c9585cd69c0ac619c
yosle <yosle007@gmail.com>
2023-08-16 17:55:15 -0400

    add Reasons type

--- interfaces/paymentlink/index.d.ts
+++ interfaces/paymentlink/index.d.ts
@@ -1,3 +1,50 @@
+type PaymentMethodElement = "EXT" | "TPP";
+export type PaymentMethods = Element | [PaymentMethodElement, PaymentMethodElement];
+declare enum Reasons {
+    HOUSING_ARRANGEMENT = 1,
+    FINANCIAL_AID = 2,
+    FAMILY_SUPPORT = 3,
+    TRAVEL_FUND = 4,
+    REAL_ESTATE_PURCHASE = 5,
+    FURNITURE_PURCHASE = 6,
+    EDUCATION = 7,
+    MEDICAL_EXPENSES = 8,
+    OTHER = 9,
+    DEBT_PAYMENT = 10,
+    TOURISM = 11,
+    SELF_SHIPPING = 12,
+    ACCOUNT_ACTIVATION = 13,
+    SPORTS_ACTIVITIES = 14,
+    DONATION = 15,
+    AFFILIATE_COMMISSION = 16,
+    SALARY = 17,
+    SAVINGS = 18,
+    RENT_AND_LEASING = 19,
+    SHARED_EXPENSES = 20,
+    SERVICE_PAYMENT = 21,
+    GIFT = 22,
+    CRYPTOCURRENCY_PURCHASE = 23,
+    OPERATING_EXPENSES = 24,
+    CURRENCY_EXCHANGE = 25,
+    ACCOMMODATION = 26,
+    EQUIPMENT_PURCHASE = 27,
+    CONSULTING = 28,
+    SOFTWARE_DEVELOPMENT = 29,
+    REFUND = 30,
+    PACKAGE_DELIVERY = 31,
+    PERSONAL_EXPENSES = 32,
+    INVESTMENT = 33,
+    BILL_PAYMENT = 34,
+    HOST_PAYMENT = 35,
+    TRANSPORTATION = 36,
+    LOAN = 37,
+    BONUS = 38,
+    HALLOWEEN_REMITTANCE = 39,
+    PLINK_REMITTANCE = 41,
+    R1_2023 = 78,
+    HAPPYWEEK = 79,
+    SENDING_YOU_A_KISS = 80
+}
 export interface PaymentLinkPayload {
     reference: string;
     concept: string;
@@ -6,14 +53,17 @@ export interface PaymentLinkPayload {
     currency: string;
     description: string;
     singleUse: boolean;
-    reasonId: number;
+    reasonId: Reasons;
     expirationDays: number;
     lang: string;
     urlSuccess: string;
     urlFailed: string;
     urlNotification: string;
     serviceDate: string;
-    client: {
+    /**
+     * fgfsgdfgdfgd
+     */
+    client?: {
         name: string;
         lastName: string;
         address: string;
@@ -21,9 +71,9 @@ export interface PaymentLinkPayload {
         email: string;
         countryId: number;
         termsAndConditions: boolean;
-    };
-    directPayment: boolean;
-    paymentMethods?: string[];
+    } | null;
+    directPayment?: boolean;
+    paymentMethods?: PaymentMethods;
 }
 export interface PaymentLink extends PaymentLinkPayload {
     expirationDate: string;
@@ -34,3 +84,4 @@ export interface PaymentLink extends PaymentLinkPayload {
     shortUrl: string;
     paymentUrl: string;
 }
+export {};
