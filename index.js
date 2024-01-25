72ba41a376dc496a5277f5b8566fc9e7ae54c17e
yosle <yosle007@gmail.com>
2024-01-25 00:56:15 -0500

    build

--- index.js
+++ index.js
@@ -689,7 +689,9 @@ const SERVER_MODE = "Development"; // Move the constant here
  * @license MIT
  */
 if (typeof window !== "undefined") {
-    console.warn("DANGER This library should not be used client side, your credentials could be exposed");
+    console.warn(`⚠️ **Warning:** The Tropipay SDK should only be instantiated server-side for security reasons. 
+    Using it client-side may lead to unexpected behavior and security vulnerabilities. YOUR CREDENTIALS COULD BE EXPOSED. Check 
+    https://yosle.github.io/tropipayjs-docs/guides/installation/ for more details.`);
 }
 
 exports.ClientSideUtils = ClientSideUtils;
